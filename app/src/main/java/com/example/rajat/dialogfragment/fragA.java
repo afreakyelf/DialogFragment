339c4c84a6ec549361883dc19962cf6170158b41
afreakyelf <rajat.mittal.15cse@bml.edu.in>
2018-06-29 10:46:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rajat/dialogfragment/fragA.java
@@ -0,0 +1,23 @@
+package com.example.rajat.dialogfragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragA extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.activity_frag,null);
+        return v;
+    }
+}
